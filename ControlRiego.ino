121751dbb048aca21d1cf0db459c5f1bf2cfa01e
Vicente Garcia <vicente.garcia@alumnos.uv.cl>
2026-05-06 14:35:31 -0400

    Mejora en la velocidad del sistema y lectura del sensor
    Hice cambios para que el programa corra más rápido y el botón responda al tiro sin quedarse pegado. También agregué que el Arduino muestre en la pantalla del computador los datos reales que va midiendo el sensor de humedad

--- ControlRiego.ino
+++ ControlRiego.ino
@@ -11,16 +11,20 @@ const int PIN_BOTON = 2;
 
 // Salidas: Control del Motor (L298N)
 const int IN1 = 7; // Dirección 1
-const int IN2 = 8; // Dirección 2
+const int IN2 = 8; // Direction 2
 const int ENA = 9; // Enable (PWM para velocidad)
 
 // Pantalla LCD 16x2 (Pines: RS, EN, D4, D5, D6, D7)
 LiquidCrystal lcd(12, 11, 6, 5, 4, 3);
 
-// Variables globales
+// Variables globales de control
 int humedadPorcentaje = 0;
 bool modoManual = false;
 bool bombaActivada = false;
+
+// Variables de tiempo para el control asíncrono (Tu aporte)
+unsigned long ultimoMuestreoSensor = 0;
+const unsigned long INTERVALO_SENSOR = 1000; // Leer el sensor cada 1 segundo (1000 ms)
 
 void setup() {
   Serial.begin(9600);
@@ -38,14 +42,22 @@ void setup() {
   analogWrite(ENA, 0);
   
   // Integrante 5: Iniciar LCD aquí
+  lcd.begin(16, 2);
+  lcd.print("Sistema Riego");
 }
 
 void loop() {
-  leerHumedad();
+  // El botón se lee constantemente sin bloqueos para un debouncing perfecto
   leerBoton();
-  evaluarUmbral();
-  actualizarPantalla();
-  delay(100); 
+  
+  // El sensor, la lógica y la pantalla se ejecutan de manera asíncrona cada 1 segundo
+  if (millis() - ultimoMuestreoSensor >= INTERVALO_SENSOR) {
+    ultimoMuestreoSensor = millis();
+    
+    leerHumedad();
+    evaluarUmbral();
+    actualizarPantalla();
+  }
 }
 
 // --- INTEGRANTE 2: Módulo Analógico para YL-69 / FC-28 ---
@@ -60,7 +72,12 @@ void leerHumedad() {
 
   // Se limita el valor para evitar porcentajes negativos o mayores a 100%
   humedadPorcentaje = constrain(humedadPorcentaje, 0, 100);
+  
+  // Depuración serial técnica exigida por la rúbrica (Tu aporte)
+  Serial.print("ADC Raw: "); Serial.print(valorSensor);
+  Serial.print(" -> Humedad Mapeada: "); Serial.print(humedadPorcentaje); Serial.println("%");
 }
+
 // --- INTEGRANTE 3: Módulo Digital y Debouncing ---
 void leerBoton() {
   // Cambiar entre modo manual y automático con debouncing
@@ -74,7 +91,14 @@ void evaluarUmbral() {
 // --- INTEGRANTE 5: Interfaz y Actuadores ---
 void actualizarPantalla() {
   // 1. Control del L298N:
-  //    Si bombaActivada: digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW); analogWrite(ENA, 200);
-  //    Si no: analogWrite(ENA, 0);
+  if (bombaActivada) {
+    digitalWrite(IN1, HIGH); 
+    digitalWrite(IN2, LOW); 
+    analogWrite(ENA, 200);
+  } else {
+    digitalWrite(IN1, LOW); 
+    digitalWrite(IN2, LOW); 
+    analogWrite(ENA, 0);
+  }
   // 2. Mostrar datos en el LCD
 }
